124618f65519a5f32b6d239cc15bb30e3b34f301
Lukesh M <lukeshmurugesan@gmail.com>
2026-06-22 16:12:03 +0530

    Add SQL commands for deleting rows and tables
    Added SQL commands to delete specific rows, truncate the table, and drop the table.

---
+++ Delete_rows.sql
@@ -0,0 +1,9 @@
+-- to delete a row from the table
+DELETE FROM employees
+WHERE emp_id = 3;
+
+-- to delete all rows from table
+TRUNCATE TABLE employees;
+
+-- to delete the entire table
+DROP TABLE employees;
