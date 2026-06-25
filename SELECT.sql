b76b8746fd23cf2ff39229bb1bdaa40adfcfe393
Lukesh M <lukeshmurugesan@gmail.com>
2026-06-25 20:01:26 +0530

    Add SELECT queries for employees table
    Add SQL queries to select data from employees table

---
+++ SELECT.sql
@@ -0,0 +1,11 @@
+-- to select all in the table
+SELECT *
+FROM employees;
+
+-- to select a single column
+SELECT emp_name
+FROM employees;
+
+-- to select multiple columns
+SELECT emp_id, emp_name
+FROM employees;
